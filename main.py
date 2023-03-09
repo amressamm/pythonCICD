8969dcbc686ba0628a37cefed03e41f7b669072a
Amr Essam <amressamm@gmail.com>
2023-03-09 03:22:14 +0200

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def main(num1: int, num2: int):
+def abcmain(num1: int, num2: int):
     num3: int = num1 + num2
     return num3
 
 
-res = main(1,3)
+res = abcmain(1, 3)
 print(res)
---
+++ test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from main import abcmain
+
+class Testing(unittest.TestCase):
+    def test_string(self):
+        abcmain(1,2)
+        a = 'some'
+        b = 'some'
+        self.assertEqual(a, b)
+
+    def test_boolean(self):
+        a = True
+        b = True
+        self.assertEqual(a, b)
+
+
+if __name__ == '__main__':
+    unittest.main()
